a6d26ce393ad2f8ca732d580eab669e7022a3c27
Kristaps Vecvagars <vecvagars.kristaps@gmail.com>
2019-07-27 11:45:05 +0300

    added div height controls and fizz/buzz input fields

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function myFizzBuzz() {
+
+    const divWidth = document.getElementById('div_width');
+    const divHeight = document.getElementById('div_height');
+    const fizzValue = document.getElementById('fizz_value').value;
+    const buzzValue = document.getElementById('buzz_value').value;
+
+    if (divWidth.value == '') {
+        return alert('Please enter div width!')
+    };
+
+    if (divHeight.value == '') {
+        return alert('Please enter div height!')
+    };
+
     const myContainer = document.createElement('section');
     myContainer.id = "container";
     document.getElementById('main').appendChild(myContainer);
@@ -8,18 +22,20 @@ function myFizzBuzz() {
         const myDiv = document.createElement('div');
         myDiv.id = "block_ID_" + i;
         myDiv.innerText = i;
+        myDiv.style.width = divWidth.value + 'px';
+        myDiv.style.height = divHeight.value + 'px';
 
-        if (i % 3 === 0) {
+        if (i % fizzValue === 0) {
             myDiv.innerText += " FIZZ";
             myDiv.className = "fizz_class";
         };
 
-        if (i % 5 === 0) {
+        if (i % buzzValue === 0) {
             myDiv.innerText += " BUZZ";
             myDiv.className += " buzz_class";
         };
 
-        if (i % 3 === 0 && i % 5 === 0) {
+        if (i % fizzValue === 0 && i % buzzValue === 0) {
             myDiv.className = "both_class";
         };
 

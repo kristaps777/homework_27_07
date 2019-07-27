d51f67af129b64f6c78391b8d7b7044dd4766493
Kristaps Vecvagars <vecvagars.kristaps@gmail.com>
2019-07-27 13:13:46 +0300

    added error checking, background image and some alignment styles

--- script.js
+++ script.js
@@ -4,24 +4,63 @@ function myFizzBuzz() {
     const divHeight = document.getElementById('div_height');
     const fizzValue = document.getElementById('fizz_value').value;
     const buzzValue = document.getElementById('buzz_value').value;
+    const errorMsg = document.getElementById('error_messages');
+    const mainContainer = document.getElementById('main');
+    let startValue = document.getElementById('start_value').value;
+    let endValue = document.getElementById('end_value').value;
+    let sectionCheck = mainContainer.hasChildNodes();
 
-    if (divWidth.value == '') {
-        return alert('Please enter div width!')
+
+    // error check switch
+    switch (true) {
+        case divWidth.value == '':
+            errorMsg.innerText = '';
+            return errorMsg.innerText += "-- enter div width --";
+            break;
+
+        case divWidth.value == 0:
+            errorMsg.innerText = '';
+            return errorMsg.innerText += "-- div width can't be zero--";
+            break;
+
+        case divWidth.value < 0:
+            errorMsg.innerText = '';
+            return errorMsg.innerText += "-- div width can't be negative--";
+            break;
+
+        case divHeight.value == '':
+            errorMsg.innerText = '';
+            return errorMsg.innerText += "-- enter div height --";
+            break;
+
+        case divHeight.value == 0:
+            errorMsg.innerText = '';
+            return errorMsg.innerText += "-- div height can't be zeroc --";
+            break;
+
+        case divHeight.value < 0:
+            errorMsg.innerText = '';
+            return errorMsg.innerText += "-- div height can't be negative --";
+            break;
     };
 
-    if (divHeight.value == '') {
-        return alert('Please enter div height!')
+    if (sectionCheck) {
+        return errorMsg.innerText += "-- already generated --";
     };
+
+    errorMsg.innerText = '';
 
     const myContainer = document.createElement('section');
     myContainer.id = "container";
     document.getElementById('main').appendChild(myContainer);
 
-    for (let i = 1; i < 101; i++) {
+    for (let i = startValue; i <= endValue; i++) {
 
         const myDiv = document.createElement('div');
+        const myBr = document.createElement('br');
         myDiv.id = "block_ID_" + i;
         myDiv.innerText = i;
+        myDiv.appendChild(myBr);
         myDiv.style.width = divWidth.value + 'px';
         myDiv.style.height = divHeight.value + 'px';
 
@@ -46,5 +85,7 @@ function myFizzBuzz() {
 
 function myDestroyer() {
     const target = document.getElementById('container');
+    const errorMsg = document.getElementById('error_messages');
     target.parentNode.removeChild(target);
+    errorMsg.innerText = '';
 };
